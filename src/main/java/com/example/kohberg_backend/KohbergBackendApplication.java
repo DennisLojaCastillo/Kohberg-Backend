6c4c9d61481ea1048b5d1bd8b27f022dd3b50e42
DennisLojaCastillo <dennislojacastilllo@gmail.com>
2023-11-20 10:53:34 +0100

    Initial commit

---
+++ src/main/java/com/example/kohberg_backend/KohbergBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kohberg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KohbergBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KohbergBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kohberg_backend/KohbergBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kohberg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KohbergBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
